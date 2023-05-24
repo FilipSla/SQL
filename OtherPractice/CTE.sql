764adddcdfb114883c7cae3fbb23ccabcd8b35cb
Filip Slanina <10fili10@seznam.cz>
2023-05-24 13:54:47 +0200

    Repair of CTE

--- OtherPractice/CTE.sql
+++ OtherPractice/CTE.sql
@@ -1,21 +1,11 @@
-;WITH
-    manIds
-    AS
-    (
-        SELECT
-            ManufacturerID
-        FROM
-            Product p
-            INNER JOIN Sales s ON p.ProductID = s.ProductID
-        WHERE
-    p.Category = 'Rural'
-        GROUP BY
-    ManufacturerID
-    )
-SELECT
-    *
-FROM
-    Manufacturer m 
-    LEFT JOIN manIds mi on m.ManufacturerID = mi.ManufacturerID
-WHERE
-    mi.ManufacturerID IS NULL;
+WITH CTE_manIDs AS (
+    SELECT ManufacturerID
+    FROM Product p
+        INNER JOIN Sales s ON p.ProductID = s.ProductID
+    WHERE p.Category = 'Rural'
+    GROUP BY ManufacturerID
+)
+SELECT *
+FROM Manufacturer m
+    LEFT JOIN CTE_manIDs mi ON m.ManufacturerID = mi.ManufacturerID
+WHERE mi.ManufacturerID IS NULL;
